a0d51ff3aeeec2b3dc31f53023946464af237fc7
Steffen Klaue <steffen.klaue@gmail.com>
2020-06-17 18:27:53 +0200

    fading audiofile in and out, storing audio playback time and seek on reentry

--- App.js
+++ App.js
@@ -1,9 +1,6 @@
 /**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
+ * Pèlerinage Sonore
+ * … it will be great.
  */
 
 import React from 'react';
@@ -19,86 +16,106 @@ import {
 import BackgroundGeolocation from "react-native-background-geolocation";
 import Video from 'react-native-video';
 
-
 export default class App extends React.Component {
+
+  player;
+
   constructor(props) {
     super(props)
     this.state = {
+      title: "Pèlerinage Sonore",
       active_geofence: null,
+      rewindGap: 7,
+      isFading: false,
+      requestId: null, 
       data: {
         '0': {
           'uri': 'http://dispatchwork.info/peso/0.mp3',
           'title': 't10',
-          'paused': true
+          'paused': true,
+          'seek': 0,
+          'volume': 0.5
         },
         '1': {
           'uri': 'http://dispatchwork.info/peso/1.mp3',
           'title': 'spektrum',
-          'paused': true
+          'paused': true,
+          'seek': 0,
+          'volume': 0.5
         },
         '2': {
           'uri': 'http://dispatchwork.info/peso/2.mp3',
           'title': 'hornstr',
-          'paused': true
+          'paused': true,
+          'seek': 0,
+          'volume': 0.5
         },
         '3': {
           'uri': 'http://dispatchwork.info/peso/3.mp3',
           'title': 'wartenburg',
-          'paused': true
+          'paused': true,
+          'seek': 0,
+          'volume': 0.5
         },
         '4': {
           'uri': 'http://dispatchwork.info/peso/4.mp3',
           'title': 'gretchen',
-          'paused': true
+          'paused': true,
+          'seek': 0,
+          'volume': 0.5
         },
         '5': {
           'uri': 'http://dispatchwork.info/peso/5.mp3',
           'title': 'obentraut',
-          'paused': true
+          'paused': true,
+          'seek': 0,
+          'volume': 0.5
         },
         '6': {
           'uri': 'http://dispatchwork.info/peso/6.mp3',
           'title': 'ruhldorfer',
-          'paused': true
+          'paused': true,
+          'seek': 0,
+          'volume': 0.5
         },
         '7': {
           'uri': 'http://dispatchwork.info/peso/7.mp3',
-          'title': 'hasenheide',
-          'paused': true
+          'title': 'hasenheide rampe',
+          'paused': true,
+          'seek': 0,
+          'volume': 0.5
+        },
+        '8': {
+          'uri': 'http://dispatchwork.info/peso/8.mp3',
+          'title': 'hasenheide feld',
+          'paused': true,
+          'seek': 0,
+          'volume': 0.5
+        },
+        '9': {
+          'uri': 'http://dispatchwork.info/peso/9.mp3',
+          'title': 'tempelufer/großbeeren',
+          'paused': true,
+          'seek': 0,
+          'volume': 0.5
         }
       }
     }
   }
   componentDidMount() {
-    ////
-    // 1.    Wire up event-listeners
-    //
-
-    // This handler fires whenever bgGeo receives a location update.
     BackgroundGeolocation.onLocation(this.onLocation, this.onError);
-
-    // This handler fires when movement states changes (stationary->moving; moving->stationary)
+    
     BackgroundGeolocation.onMotionChange(this.onMotionChange);
-
-    // This event fires when a change in motion activity is detected
     BackgroundGeolocation.onActivityChange(this.onActivityChange);
-
-    // This event fires when the user toggles location-services authorization
     BackgroundGeolocation.onProviderChange(this.onProviderChange);
 
     BackgroundGeolocation.onGeofence((event) => this.onGeofence(event, this));
 
-    ////
-    // 2.    Execute #ready method (required)
-    //
     BackgroundGeolocation.ready({
-      // Geolocation Config
       desiredAccuracy: BackgroundGeolocation.DESIRED_ACCURACY_HIGH,
       distanceFilter: 10,
-      // Activity Recognition
       stopTimeout: 1,
-      // Application config
-      debug: true, // <-- enable this hear sounds for background-geolocation life-cycle.
+      debug: false, // <-- enable this hear sounds for background-geolocation life-cycle.
       logLevel: BackgroundGeolocation.LOG_LEVEL_VERBOSE,
       stopOnTerminate: false,     // <-- Allow the background-service to continue tracking when user closes the app.
       startOnBoot: true,                // <-- Auto start tracking when device is powered-up.
@@ -113,15 +130,22 @@ export default class App extends React.Component {
         "auth_token": "maybe_your_server_authenticates_via_token_YES?"
       }
     }, (state) => {
-      console.log("- BackgroundGeolocation is configured and ready: ", state.enabled);
+      console.log("[BackgroundGeolocation is configured and ready]", state.enabled);
+      
+      fetch('https://dispatchwork.info/peso/peso-data.json')
+      .then(response => response.json())
+      .then(data => {
+        //console.log(JSON.stringify(data.geofence, undefined, 2))
+        //console.log(JSON.stringify(data.geofenceData, undefined, 2))
+        console.log(data.geofenceData)
+      })
+      .catch(error => console.error(error));
+
       this.setupGeofences();
 
       if (!state.enabled) {
-        ////
-        // 3. Start tracking!
-        //
         BackgroundGeolocation.start(function() {
-          console.log("- Start success");
+          console.log("[Start BackgroundGeolocation]");
         });
       }
     });
@@ -186,48 +210,67 @@ export default class App extends React.Component {
         notifyOnExit: true
       },
       {
-        identifier: "7", // hasenheide
-        radius: 50,
+        identifier: "7", // hasenheide rampe
+        radius: 100,
         latitude: 52.487623,
         longitude: 13.414109,
         notifyOnEntry: true,
         notifyOnExit: true
-      }
+      },
+      {
+        identifier: "8", // hasenheide feld
+        radius: 100,
+        latitude: 52.481445,
+        longitude: 13.419373,
+        notifyOnEntry: true,
+        notifyOnExit: true
+      },
+      {
+        identifier: "9", // tempelufer/großbeeren
+        radius: 50,
+        latitude: 52.498665,
+        longitude: 13.383811,
+        notifyOnEntry: true,
+        notifyOnExit: true
+      },
     ]).then(success => console.log('Added Geofences')).catch(err => console.error(err))
   }
-
-  // You must remove listeners when your component unmounts
   componentWillUnmount() {
     BackgroundGeolocation.removeListeners();
   }
   onLocation(location) {
-    console.log('[location] -', location);
+    console.log('[location]', location);
   }
   onError(error) {
-    console.warn('[location] ERROR -', error);
+    console.warn('[location] ERROR', error);
   }
   onActivityChange(event) {
-    console.log('[activitychange] -', event);    // eg: 'on_foot', 'still', 'in_vehicle'
+    console.log('[activitychange]', event);    // eg: 'on_foot', 'still', 'in_vehicle'
   }
   onProviderChange(provider) {
-    console.log('[providerchange] -', provider.enabled, provider.status);
+    console.log('[providerchange]', provider);
   }
   onMotionChange(event) {
-    console.log('[motionchange] -', event.isMoving, event.location);
+    console.log('[motionchange]', event.isMoving, event.location);
   }
   onGeofence(geofence, self) {
-    console.log("[geofence] ", geofence.identifier, geofence.action);
+    console.log('[geofence]', geofence.action, geofence.identifier);
+
     if (Object.keys(this.state.data).indexOf(geofence.identifier) < 0) {
       return
     }
+    if(this.state.isFading){
+      cancelAnimationFrame(this.state.requestId)
+    }
+
     if ('ENTER' == geofence.action) {
       self.state.active_geofence = geofence.identifier
-      self.state.data[geofence.identifier].paused = false;
+      self.state.data[geofence.identifier].paused = false
+
+      this.fadeIn()
     } else {
-      self.state.active_geofence = null
-      self.state.data[geofence.identifier].paused = true;
+      this.fadeOut()
     }
-    self.forceUpdate()
   }
   getVideoProps() {
     const active = this.state.data[this.state.active_geofence]
@@ -236,13 +279,90 @@ export default class App extends React.Component {
     } else {
       return {
         identifier: "-1",
-        title: 'None'
+        title: 'None',
+        uri: '.'
       }
     }
   }
+  onVideoStart(data) {
+    let activeSeek = this.state.data[this.state.active_geofence].seek,
+        rewindSeek = (activeSeek >= this.state.rewindGap) ? activeSeek-this.state.rewindGap : data.currentTime
+    
+    this.player.seek(rewindSeek)
+  }
+  onVideoProgress(data) {
+    if (Object.keys(this.state.data).indexOf(this.state.active_geofence) < 0) {
+      return
+    }
+    
+    this.state.data[this.state.active_geofence].seek = data.currentTime
+  }
+  onVideoEnd(data) {
+    this.player.seek(0)
+  }
+  fadeIn() {
+    let duration = 3000,
+        end = new Date().getTime() + duration
+    
+    this.state.isFading = true
+
+    this.doFadeIn(duration, end)
+  }
+
+  doFadeIn(duration, end) {
+    const fn = () => {
+      let current = new Date().getTime(),
+          remaining = end - current,
+          t = 1 - remaining / duration
+
+          if(t >= 1){
+            this.state.isFading = false
+            return
+          }
+
+          this.state.data[this.state.active_geofence].volume = t
+
+          this.forceUpdate()
+          this.doFadeIn(duration, end)
+    }
+    this.state.requestId = requestAnimationFrame(fn)
+  }
+
+  fadeOut() {
+    let duration = 5000,
+        end = new Date().getTime() + duration
+
+    this.state.isFading = true
+
+    this.doFadeOut(duration, end)
+  }
+
+  doFadeOut (duration, end) {
+    const fn = () => {
+      let current = new Date().getTime(),
+          remaining = end - current,
+          t = remaining / duration
+
+      if (t <= 0) {
+        this.state.isFading = false
+        this.state.data[this.state.active_geofence].paused = true
+        this.state.active_geofence = null
+        this.forceUpdate()
+        return
+      }
+      this.state.data[this.state.active_geofence].volume = t
+
+      this.forceUpdate()
+      this.doFadeOut (duration, end)
+    }
+
+    this.state.requestId = requestAnimationFrame(fn)
+  }
+
   render() {
     const active = this.getVideoProps()
-    console.log(active)
+    console.log("[render]", active)
+
     return (
       <>
         <StatusBar barStyle="dark-content" />
@@ -250,14 +370,20 @@ export default class App extends React.Component {
           <ScrollView>
             <View>
               <Text>{active.title}</Text>
-              <Video source={{uri: active.uri}}   // Can be a URL or a local file.
+
+              <Video 
+                source={{uri: active.uri}}
                 audioOnly={true}
                 controls={true}
                 playInBackground={true}
+                playWhenInactive={true}
                 paused={active.paused}
-                ref={(ref) => {
-                  this.player = ref
-                }} />
+                volume={active.volume}
+                ref={ref=>{this.player=ref}}
+                onLoad={this.onVideoStart.bind(this)}
+                onProgress={this.onVideoProgress.bind(this)}
+                onEnd={this.onVideoEnd.bind(this)}
+                />
             </View>
           </ScrollView>
         </SafeAreaView>
